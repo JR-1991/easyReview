12730997129e4c00db7fc84ea1ce944774206538
Jan Range <range.jan@web.de>
2023-12-27 01:50:03 +0100

    remove unused handlers

--- backend/api/handlers/review.py
+++ backend/api/handlers/review.py
@@ -18,44 +18,6 @@ def review_by_reviewer(request, reviewerid):
     return Response(serializer.data)
 
 
-def add_review(request):
-    serializer = ReviewSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-    else:
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-def all_reviews(request):
-    reviews = Review.objects.all()
-    serializer = ReviewSerializer(reviews, many=True)
-    return Response(serializer.data)
-
-
-def review_by_id(request, id):
-    review = Review.objects.get(pk=id)
-    serializer = ReviewSerializer(review, many=False)
-    return Response(serializer.data)
-
-
-def delete_review(request, id):
-    try:
-        Review.objects.get(pk=id).delete()
-    except Exception as e:
-        return Response(
-            {
-                "message": f"Could not delete - Review of UUID '{id}' does not exist or is not a valid UUID"
-            },
-            status=status.HTTP_400_BAD_REQUEST,
-        )
-
-    return Response(
-        {"message": f"Review '{id}' has successfully been deleted"},
-        status=status.HTTP_200_OK,
-    )
-
-
 def get_field_count(request, review_id):
     # Get total field count
     primitive_fields = list(
@@ -81,27 +43,6 @@ def get_field_count(request, review_id):
         {
             "field_count": field_count,
             "accpected_count": accepted_fields,
-        },
-        status=status.HTTP_200_OK,
-    )
-
-
-def update_review(request, id):
-    review = Review.objects.filter(pk=id)
-
-    try:
-        review.update(**request.data)
-    except FieldDoesNotExist as e:
-        return Response(
-            {
-                "message": f"Review update failed - {str(e)}",
-            },
-            status=status.HTTP_400_BAD_REQUEST,
-        )
-
-    return Response(
-        {
-            "message": "Review updated successfully",
         },
         status=status.HTTP_200_OK,
     )
