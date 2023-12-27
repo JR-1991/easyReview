07ade128bf406c0b76e7b5f6e2d718b39e1f3b93
Jan Range <range.jan@web.de>
2023-12-27 16:58:15 +0100

    added error modal

---
+++ frontend/app/components/errormodal.tsx
@@ -0,0 +1,15 @@
+import cardStyle from "../utils/styles"
+
+export default function ErrorModal(
+    {
+        error
+    }: {
+        error: string
+    }) {
+    return (
+        <div className={`modal-box p-9 ${cardStyle}`}>
+            <h3 className="text-xl font-bold">Something went wrong 😵</h3>
+            <p className="py-4 font-light">{error}</p>
+        </div>
+    )
+}
